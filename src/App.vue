3307e3e0909eabf482ed89f84e6ec5e37d66098e
Bryan Jimenez <bryanbg.jg@gmail.com>
2022-05-12 18:37:04 -0400

    feat: add vuetify table

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,136 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+     <NavbarVue /> 
+     <div class="container-fluid ps-5 pe-5">
+    <div>
+      <h5>Consulta cliente domicilio</h5>
+    </div>
+
+    <hr width="100">
+
+    <div class="row mb-3">
+      <div class="col-md-6">
+        <div class="d-flex flex-row">
+          <h6 class="search">Buscar:</h6>
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1"><i
+                class="uil uil-search"></i></span>
+            <input type="text" class="form-control" placeholder="Escribe lo que desees buscar" aria-label="Username"
+              aria-describedby="basic-addon1">
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="align-end">
+          <button class="btn btn-primary">Nuevo domicilio</button>
+
+        </div>
+      </div>
+    </div>
+    </div>
+     <TableView/>
+
+
     </v-main>
-  </v-app>
+  
 </template>
 
+<style>
+
+#app {
+  /* width: 100vw; */
+  height: 100vh;
+  font-weight: normal;
+}
+
+body {
+  box-sizing: border-box;
+
+}
+
+/* Buttons styling */
+
+.btn-edit {
+  border-radius: 5px;
+  border: none;
+  margin-left: 77%;
+  background-color: white;
+  color: blue;
+
+}
+
+.btn-edit i {
+  margin: 5px;
+}
+
+.btn-cancel {
+  border: none;
+  background-color: white;
+  color: red;
+  width: 2%;
+  border-radius: 5px;
+}
+
+h5 {
+  margin-top: 30px;
+  color: #4B4B4B;
+
+}
+
+hr {
+  background-color: black;
+  width: 100%;
+
+}
+
+h5 {
+  margin-top: 30px;
+  color: #4B4B4B;
+
+}
+
+hr {
+  background-color: black;
+  width: 100%;
+
+}
+
+.input-group {
+  padding-left: 1rem;
+}
+
+.search {
+  color: black;
+  margin-bottom: auto;
+  margin-top: auto;
+}
+
+.align-end {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+}
+
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import NavbarVue from './components/NavbarVue';
+import TableView from './components/TableView';
+
 
 export default {
   name: 'App',
+  
 
   components: {
-    HelloWorld,
-  },
+    NavbarVue,
+    TableView,
+},
 
   data: () => ({
     //
---
+++ src/components/NavbarVue.vue
@@ -0,0 +1,38 @@
+
+<template>
+<main>
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <i class="uil uil-user"></i>
+      Bryan Jimenez
+    </a>
+    <button type="button" class="btn-edit"><i class="uil uil-pen"></i>Editar</button>
+    <button type="button" class="btn-cancel"><i class="uil uil-times"></i></button>
+  </div>
+</nav>
+
+</main>
+
+</template>
+
+<style>
+nav{
+        width: 100%;
+        
+    }
+</style>
+
+<script>
+
+export default {
+
+    name: "NavbarVue",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
